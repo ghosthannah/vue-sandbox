3e619c0e0f2c9fedcc038ec29159c80b92c724cd
ghosthannah <correllohh312@gmail.com>
2020-02-21 21:44:44 -0500

    add card component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="main">
-    <h2>{{ title }}</h2>
-    <div>Welcome, {{ username }}! This is a VueJS app.</div>
+    <div class="header">
+      <h2>{{ title }}</h2>
+      <div>Welcome, {{ username }}! This is a VueJS app.</div>
+    </div>
+
+    <div
+      v-for="card in cards"
+      :key="card.title"
+      class="display-inline-block padding-between"
+    >
+      <Card
+        :title="card.title"
+        :subtitle="`${card.subtitle ? card.subtitle : ''}`"
+        :content="card.content"
+        :link="`${card.link ? card.link : ''}`"
+      />
+    </div>
   </div>
 </template>
 
@@ -12,12 +27,49 @@
   @include until-medium {
     padding: 5px;
   }
+  .header {
+    margin-bottom: 20px;
+    @include until-medium {
+      margin-bottom: 10px;
+    }
+  }
+
+  .padding-between {
+    padding: 10px;
+  }
 }
 </style>
 
 <script>
+import Card from "@/components/Card";
 export default {
   name: "App",
+  components: {
+    Card
+  },
+  computed: {
+    cards() {
+      return [
+        {
+          title: "Sample Card",
+          subtitle: "An example card",
+          content:
+            "This should accept an HTML string, so <i>try it out</i> if you wish."
+        },
+        {
+          title: "Link",
+          subtitle: "A card with a link ",
+          content:
+            "Clicking on this card should direct you to the specified link.",
+          link: "https://www.hannahcorrello.com"
+        },
+        {
+          title: "No Subtitle or Link",
+          content: "Let's hope this content is interesting on it's own!"
+        }
+      ];
+    }
+  },
   data() {
     return {
       title: "Vue Sandbox",
---
+++ src/components/Card.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="card-main">
+    <a class="card style-like-div" v-if="link" :href="link">
+      <h2 class="card-title">{{ title }}</h2>
+      <h5 class="card-subtitle" v-if="subtitle">{{ subtitle }}</h5>
+      <div class="card-content" v-if="content" v-html="content"></div>
+    </a>
+    <div v-else class="card">
+      <h2 class="card-title">{{ title }}</h2>
+      <h5 class="card-subtitle" v-if="subtitle">{{ subtitle }}</h5>
+      <div class="card-content" v-if="content" v-html="content"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.style-like-div {
+  text-decoration: none !important;
+}
+
+.style-like-div:hover {
+  background: $almost-white;
+}
+
+a:hover {
+  cursor: pointer;
+}
+
+.card {
+  padding: 20px;
+  max-width: 300px;
+  max-height: 400px;
+
+  box-shadow: 1px 1px 12px 0 rgba(0, 0, 0, 0.22);
+  background: #fff;
+  color: $navy;
+  display: flex !important;
+  flex-direction: column;
+  justify-content: space-between;
+  position: relative;
+  top: 0;
+  transition: top 0.2s ease, opacity 1s ease, transform 1.5s ease;
+
+  @include until-medium {
+    padding: 5px;
+    height: 250px;
+    width: 175px;
+  }
+
+  .card-title {
+    font-weight: $font-weight-bold;
+    color: $turquoise;
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+
+  .card-subtitle {
+    font-weight: $font-weight-normal;
+    color: $dark-teal;
+    margin-top: 5px;
+    margin-bottom: 10px;
+  }
+}
+
+.card:hover {
+  top: -8px;
+}
+</style>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    title: {
+      type: [String, Number],
+      required: true
+    },
+    subtitle: {
+      type: String,
+      required: false
+    },
+    content: {
+      type: [String, Number],
+      required: false
+    },
+    link: {
+      type: String,
+      required: false
+    }
+  }
+};
+</script>
